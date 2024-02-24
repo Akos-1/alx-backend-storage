083355e55548cab95f497bd806d668b329788bce
Akos-1 <veronicagyasi@gmail.com>
2024-02-24 12:48:08 -0800

    updated

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,9 +1,13 @@
--- SQL script to rank country origins of bands by the number of (non-unique) fans
-SELECT origin, SUM(nb_fans) AS total_fans
-FROM (
-    SELECT origin, COUNT(*) AS nb_fans
-    FROM metal_bands
-    GROUP BY origin
-) AS subquery
+-- Create a temporary table to import the data
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_metal_bands AS
+SELECT *
+FROM metal_bands;
+
+-- Query to rank country origins of bands by the number of non-unique fans
+SELECT origin, COUNT(*) AS nb_fans
+FROM temp_metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
+
+-- Drop the temporary table after use
+DROP TEMPORARY TABLE IF EXISTS temp_metal_bands;
